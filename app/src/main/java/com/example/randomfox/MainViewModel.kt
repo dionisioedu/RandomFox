c3f8b3c3807e78172442f5212cfe8685901483ee
cce_cfernandes <cce_cfernandes@uolinc.com>
2021-03-27 23:45:03 -0300

    Initial commit

---
+++ app/src/main/java/com/example/randomfox/MainViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.randomfox
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.randomfox.models.FoxModel
+import com.example.randomfox.network.FoxApi
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+    private val _random_fox = MutableLiveData<FoxModel>()
+
+    val random_fox : LiveData<FoxModel>
+        get() = _random_fox
+
+    init {
+        nextFox()
+    }
+
+    fun nextFox() {
+        viewModelScope.launch {
+            try {
+                Log.d("MainViewModel", "Calling retrofit")
+
+                val newValue = FoxApi.retrofitService.getRandomFox()
+
+                Log.d("MainViewModel", "Received new value $newValue")
+
+                _random_fox.postValue(newValue)
+
+            } catch (e: Exception) {
+                Log.e("nextFox", e.message.toString())
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/randomfox/models/FoxModel.kt
@@ -0,0 +1,6 @@
+package com.example.randomfox.models
+
+data class FoxModel(
+    val image: String?,
+    val link: String?,
+)
